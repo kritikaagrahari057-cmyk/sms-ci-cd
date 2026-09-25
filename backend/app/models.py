71fc22c50afd04e5c34a8872f9630fdce22095e7
Kritika Agrahari <kritikaagrahari057@gmail.com>
2026-09-25 10:49:31 +0530

    Add backend application

---
+++ backend/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100) , nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    age = Column(Integer, nullable=False)
+
+    
---
+++ backend/app/schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel,EmailStr
+
+class StudentCreate(BaseModel):
+    name:str
+    email:EmailStr
+    age:int
+
+    
